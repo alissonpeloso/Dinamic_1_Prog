415b1f0ba30715987d3013a6c1bfa2d7b3eb0903
Guilherme Graeff <guilherme.rafael.graeff@gmail.com>
2020-11-18 14:04:29 -0300

    add variavel, para numero

---
+++ variavel.java
@@ -0,0 +1,16 @@
+public class VarInt{
+    public int value;
+    public VarInt(String name, int value){
+        this.type = "int";
+        this.name = name;
+        this.value = value;
+    }
+    public VarInt(String name){
+        this.type = "int";
+        this.name = name;
+        this.value = 0;
+    }
+    public Integer getValue(){
+        return this.value;
+    }
+}
