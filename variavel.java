be0c57ce9fd5039b0bcddaaa20b35996f8946968
Guilherme Graeff <guilherme.rafael.graeff@gmail.com>
2020-11-18 14:12:24 -0300

    add vetor de 20, e os métodos menor, maior

--- variavel.java
+++ variavel.java
@@ -1,16 +1,32 @@
 public class VarInt{
-    public int value;
-    public VarInt(String name, int value){
-        this.type = "int";
-        this.name = name;
-        this.value = value;
+    public int values[20];
+    public int maior;
+    public int menor;
+    
+    public guardaInteiros(){
+        for (int i =0; i<20;i++){
+            System.out.printf("Informe o númeron");
+            values[i] = ler.nextInt();
+        }
     }
-    public VarInt(String name){
-        this.type = "int";
-        this.name = name;
-        this.value = 0;
+    public int menor(){
+        for (int i = 0; i < 20; i++){
+            if (valor[i]< menor){
+                menor = values[i];
+            }
+        }
+        return(menor);
     }
-    public Integer getValue(){
-        return this.value;
+
+
+
+    
+    public int maior(){
+        for(int i=0 ; i<20 ; i++){
+            if(values[i]>maior){
+                maior = values[i];
+            }
+        }
+        return maior;
     }
 }
